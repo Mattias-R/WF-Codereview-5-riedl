a10fd5f0f993dc4f92e889fc8076ed5cc902334a
Mattias-R <mattias.r@gmx.at>
2019-10-26 21:28:52 +0200

    added sort function

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ let favorite = [];
 window.onload = function(){
 let output = document.querySelector(".js");
 let out = document.querySelector(".bs");
+let order = [];
 
-
-
+//--------------THIS CREATES THE WHOLE CONTENT ON THE WEBSITE ---------------------------------------
 	for(cart of shoppingCart){
 		output.innerHTML += 
 		`
@@ -31,20 +31,18 @@ let out = document.querySelector(".bs");
 					</div>
 				</div>
 				`
-
-				
+				order.push(this.cart);
+				console.log(order);
 	
 	}
 
-
+//------------THIS GIVES THE BUTTON A FUNCTION - TO CREATE NEW CONTENT ----------------------------
 	let buttons = document.querySelectorAll(".add");
-	console.log(buttons);
 	for(button of buttons){
 		button.onclick=function(e){
 			favorite.push(this.id)
 			
 			    $(this).toggleClass('myclass');
-			    
 			    if($(this).hasClass("myclass")) {
 			out.innerHTML += 
 		`
@@ -83,7 +81,7 @@ let out = document.querySelector(".bs");
 	delete favorite[this]
 }
 
-
+//----------THIS REMOVES THE NEW CONTENT AND REMOVES THE GREEN BUTTON --
 		let removeButton = document.querySelectorAll(".rem");
 		for(button of removeButton){
 			button.onclick=function(e){
@@ -97,20 +95,42 @@ let out = document.querySelector(".bs");
 
 	}	
 
+//----------SORT FUNCTION------------------------
+var b = document.getElementById("bn");
+b.onclick=function(e){
+	order.sort(function(a, b){return b.price-a.price});
+	console.log(order);
+	$(".ownBorder").remove();
+	for(orders of order){
+	output.innerHTML += 		`
+			<div class="col-lg-3 offset-lg-1 col-md-5 offset-md-1 ownBorder pt-3">
+				<div class="row">
+					<div class="col-lg-12 col-mg-12">
+						<img class="image imageBorder"src="${orders.img}">
+					</div>
+						<div class="col-lg-6 col-md-6 mt-2">
+							<p>${orders.title}</p>
+						</div>
+						<div class="col-lg-3 offset-lg-3 col-md-3 offset-md-3 mt-2">
+							<p>€${orders.price}</p>
+						</div>
+						<div class="col-lg-12">
+							<p>${orders.description}</p>
+						</div>
+						<div class="col-lg-7 col-md-7">
+							<button id="${orders.id}" type="button" class=" btn btn-primary btn-sm btn-sz add">Add to favorites</button>
+						</div>
+						<div class="col-lg-5 col-md-5">
+							<p>Nothing</p>
+						</div>
+					</div>
+				</div>
+				`;
+}
 
-// function myFunction() {
-// 					  // Get the checkbox
-// 					  var checkBox = document.getElementById("myCheck");
-// 					  // Get the output text
-// 					  var text = document.getElementById("text");
+}
 
-// 					  // If the checkbox is checked, display the output text
-// 					  if (checkBox.checked == true){
-// 					    text.style.display = "block";
-// 					  } else {
-// 					    text.style.display = "none";
-// 					  }
-// 					}
+
 };
 
 //https://www.bootstraptoggle.com/
