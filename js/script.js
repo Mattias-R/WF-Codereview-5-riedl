2e280a27791cc87014aa12bc9bc8eed22406079d
Mattias-R <mattias.r@gmx.at>
2019-10-26 19:59:48 +0200

    done

--- js/script.js
+++ js/script.js
@@ -40,18 +40,16 @@ let out = document.querySelector(".bs");
 	let buttons = document.querySelectorAll(".add");
 	console.log(buttons);
 	for(button of buttons){
-		let i = 0;
-		let producId="#a" + i;
 		button.onclick=function(e){
 			favorite.push(this.id)
-
+			
 			    $(this).toggleClass('myclass');
 			    
 			    if($(this).hasClass("myclass")) {
 			out.innerHTML += 
 		`
 					<div class="col-lg-5 offset-lg-1 col-md-11 offset-md-1 ownBorder pt-3 remove${this.id}">
-				<div class="row" id="${producId}">
+				<div class="row">
 					<div class="col-lg-12 col-mg-12">
 						<img class="imageForResponsive imageBorder"src="${shoppingCart[this.id].img}">
 					</div>
@@ -71,14 +69,14 @@ let out = document.querySelector(".bs");
 							<p>${shoppingCart[this.id].description}</p>
 						</div>
 						<div class="col-lg-7 col-md-7">
-							<button id="${cart.remId} type="button" class="btn btn-primary btn-sm btn-sz rem">Remove</button>
+							<button id="${shoppingCart[this.id].id}" type="button" class="btn btn-primary btn-sm btn-sz rem">Remove</button>
 						</div>
 						<div class="col-lg-5 col-md-5 ">
 							<p>Description</p>
 						</div>
 					</div>
 				</div>
-		`
+		`;
 
 }else{
 	$(`.remove${this.id}`).remove();
@@ -90,8 +88,7 @@ let out = document.querySelector(".bs");
 		for(button of removeButton){
 			button.onclick=function(e){
  				$(this.parentNode.parentNode.parentNode).remove();
- 				// $("#16").toggleClass('myclass');
-				$(`#${this}`).removeClass("myclass")
+ 				$(`#${this.id}`).removeClass("myclass")
 			}
 		}	
 		};
